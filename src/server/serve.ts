0cc6dd0a38a8d62c58bdc1aad80415604d7131c0
Daniel Lamando <dan@danopia.net>
2025-09-20 02:14:23 +0200

    v0.1.7: Return DdpSocketSession from serveWebsocket

--- src/server/serve.ts
+++ src/server/serve.ts
@@ -5,22 +5,29 @@ export function serveWebsocket(
   req: Request,
   connInfo: Deno.ServeHandlerInfo,
   ddpInterface: DdpInterface,
-): Response {
+): {
+  ddp: DdpSocketSession | null;
+  response: Response;
+} {
+
   const upgrade = req.headers.get("upgrade") ?? "";
   if (upgrade.toLowerCase() != "websocket") {
-    return new Response("request isn't trying to upgrade to websocket.", {
-      status: 400,
-    });
+    return {
+      ddp: null,
+      response: new Response(
+        "request isn't trying to upgrade to websocket.",
+        { status: 400 }),
+    };
   }
 
   const { socket, response } = Deno.upgradeWebSocket(req);
   const ddp = new DdpSocketSession(
     socket,
     ddpInterface,
-    'raw',
+    req.url.includes('/sockjs/') ? 'sockjs' : 'raw',
     connInfo.remoteAddr,
     Object.fromEntries(req.headers),
   );
   ddp.closePromise?.then(() => {}, () => {});
-  return response;
+  return { ddp, response };
 }
